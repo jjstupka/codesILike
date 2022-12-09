20407f4ebf9bbe8da019fa45f2feb06cb3c8f3ef
João Stupka Junior <68610392+jjstupka@users.noreply.github.com>
2022-12-09 12:34:42 -0300

    Rename and update
    Correction code with name

--- spiral.py
+++ spiral.py
@@ -5,7 +5,7 @@ import random
 speed(speed='fastest')
 
 def draw(n, x, angle):
-    # loop para numero de estrelas
+    # loop para numero de espirais
     for i in range(n):
 
         colormode(255)
@@ -21,9 +21,9 @@ def draw(n, x, angle):
         # e preenchimento
         pencolor(0, 0, 0)
         fillcolor(a, b, c)
-        # começa a pintar as estrelas
+        # começa a pintar as espirais
         begin_fill()
-        # loop para cada estrela
+        # loop para cada espiral
         for j in range(20):
             turtle.circle(20 * j)
             turtle.left(7)
@@ -31,13 +31,13 @@ def draw(n, x, angle):
         # completar preenchimento
         end_fill()
 
-        # rotação na nova estrela
+        # rotação na nova espiral
         
         rt(angle)
 
 # parametros
-n = 30  # numero de estrelas
-x = 144  # angulo exterior de cada estrela
+n = 30  # numero de espirais
+x = 144  # angulo exterior de cada espiral
 angle = 60  # angulo de rotação 
 
 draw(n, x, angle)
